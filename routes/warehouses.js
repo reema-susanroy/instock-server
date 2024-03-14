11c998e5fa5cf4be7b12ead66f3830b3d3a8ed68
Heppner <Heppner@Rebekahs-MacBook-Pro.local>
2024-03-14 09:45:03 -0700

    Fix conflicts

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,38 +1,26 @@
-const express = require('express');
-const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
 
-const warehouseController = require('../controllers/warehouseController');
+const warehouseController = require("../controllers/warehouseController");
 
-
-router
-    .route("/")
-    .get(warehouseController.getWarehouses);
-    // .post
+router.route("/").get(warehouseController.getWarehouses);
+// .post
 
 router
-    .route("/:id")
-<<<<<<< HEAD
-    .get(warehouseController.findOne)
-    .put(warehouseController.update)
+  .route("/:id")
 
-=======
-<<<<<<< HEAD
-    .get(warehouseController.findOne);
+  // .get(warehouseController.findOne)
+  .put(warehouseController.update);
 
 router
-.route("/api/warehouses/:warehouseId")
-.put(warehouseController.update)
-=======
-    .get(warehouseController.findOne)
-    .delete(warehouseController.deleteWarehouse);
-   /* .put
+  .route("/api/warehouses/:warehouseId")
+  .put(warehouseController.update)
+  .get(warehouseController.findOne)
+  .delete(warehouseController.deleteWarehouse);
+/* .put
     .delete*/
->>>>>>> 716192339c5b643464d6dd5f6f78c16f167fa6fe
->>>>>>> develop
-
-    
 
 // router
 //     .route("/:warehouseId/edit")
@@ -41,7 +29,5 @@ router
 //     router
 //     .route("/:warehouseId/add")
 //     .post
-
-
 
 module.exports = router;

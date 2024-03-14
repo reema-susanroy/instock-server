9e8c31d02516ec6deb4a1fc4197bb4c12efc91c5
serenasunyu <129626024+serenasunyu@users.noreply.github.com>
2024-03-14 01:59:46 -0400

    Add email and phone number validator library

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,4 +1,6 @@
 const knex = require('knex')(require('../knexfile'));
+const { parsePhoneNumberFromString } = require('libphonenumber-js');
+const validator = require('email-validator');
 
 const findOne = async (req, res) => {
     try {
@@ -20,6 +22,76 @@ const findOne = async (req, res) => {
     }
   };
 
+  // Define validation functions
+const validateWarehouseData = (data) => {
+  const {
+    warehouse_name,
+    address,
+    city,
+    country,
+    contact_name,
+    contact_position,
+    contact_phone,
+    contact_email
+  } = data;
+  
+  console.log(data);
+
+
+  // Check if all required fields are present
+  if (!warehouse_name || !address || !city || !country ||
+      !contact_name || !contact_position || !contact_phone || !contact_email) {
+    console.log('All fields are required.');
+    return false;
+  }
+
+  // Validate phone number 
+  const phoneNumber = parsePhoneNumberFromString(contact_phone);
+  if (!(phoneNumber && phoneNumber.isValid())) {
+    console.log('Invalid phone number.');
+    return false;
+  }
+
+  // Validate email address
+  if (!validator.validate(contact_email)) {
+    console.log('Invalid email address.');
+    return false;
+  }
+  return true;
+};
+
+const update = async (req, res) => {
+    try {
+      const isValid = validateWarehouseData(req.body);
+      if (!isValid) {
+        return res.status(400).json({ 
+          message: `Invalid data for warehouse with ID ${req.params.id}` 
+        });
+      }
+
+      const affectedRows = await knex('warehouses')
+        .where({ id: req.params.id })
+        .update(req.body);
+
+      if (affectedRows === 0) {
+        return res.status(404).json({ 
+          message: `Warehouse with ID ${req.params.id} not found`
+        });
+      }
+
+      const updatedWarehouse = await knex('warehouses')
+        .where({ id: req.params.id })
+
+      res.status(200).json(updatedWarehouse[0]);
+    } catch (error) {
+      console.error('Error updating warehouse:', error);
+      res.status(400).json({ 
+        message: `Unable to update user with ID ${req.params.id}` 
+      });
+    }
+  };
+
   module.exports = {
-    findOne
+    findOne,
+    update,
   }
--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,27 +1,27 @@
-const express = require('express')(require('../knexfile'));;
+const express = require('express')(require('../knexfile'));
 const router = require('express').Router();
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 const warehouseController = require('../controllers/warehouseController');
 
 
-router
-    .route("/warehouses")
-    .get
-    .post
+// router
+//     .route("/warehouses")
+//     .get
+//     .post
 
-router
-    .route("/warehouses/:id")
-    .get(warehouseController.findOne);
-   /* .put
-    .delete*/
+// router
+//     .route("/warehouses/:id")
+//     .get(warehouseController.findOne);
+//    /* .put
+//     .delete*/
 
     
 
-// router
-//     .route("/warehouses/:warehouseId/edit")
-//     .put
+router
+    .route("/api/warehouses/:warehouseId")
+    .put(warehouseController.update)
 
 //     router
 //     .route("/warehouses/:warehouseId/add")
